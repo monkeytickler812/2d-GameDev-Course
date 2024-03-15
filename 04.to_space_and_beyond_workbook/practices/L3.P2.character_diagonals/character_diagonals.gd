de2dc392608b8a61f2ed15627bf5c48501010d77
nmbmonkey <jazhu8@s.sfusd.edu>
2024-03-15 13:05:38 -0700

    L3 and practices complete

--- 04.to_space_and_beyond_workbook/practices/L3.P2.character_diagonals/character_diagonals.gd
+++ 04.to_space_and_beyond_workbook/practices/L3.P2.character_diagonals/character_diagonals.gd
@@ -10,6 +10,8 @@ var direction := Vector2(0, 0)
 func _process(delta: float) -> void:
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
+	if direction.length() > 1.0:
+		direction = direction.normalized()
 
 	# The character is way too fast, but only when moving diagonally!
 	# Add code to prevent that.
