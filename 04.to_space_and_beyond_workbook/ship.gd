cafecbde3206ae2153f736b7ee4b49e77106ecf1
nmbmonkey <jazhu8@s.sfusd.edu>
2024-03-18 11:36:51 -0700

    L4 and practice complete

--- 04.to_space_and_beyond_workbook/ship.gd
+++ 04.to_space_and_beyond_workbook/ship.gd
@@ -1,6 +1,9 @@
 extends Sprite2D
 
-var max_speed := 600.0
+var boost_speed := 1500.0
+var normal_speed := 600.0
+
+var max_speed := normal_speed
 var velocity := Vector2(0, 0)
 
 
@@ -9,13 +12,19 @@ func _process(delta: float) -> void:
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
 
-	# If we don't do this and the player moves diagonally, they will move 40%
-	# faster than normal.
 	if direction.length() > 1.0:
 		direction = direction.normalized()
+
+	if Input.is_action_just_pressed("boost"):
+		max_speed = boost_speed
+		get_node("Timer").start()
 
 	velocity = direction * max_speed
 	position += velocity * delta
 
 	if direction.length() > 0.0:
 		rotation = velocity.angle()
+
+
+func _on_timer_timeout() -> void:
+	max_speed = normal_speed
